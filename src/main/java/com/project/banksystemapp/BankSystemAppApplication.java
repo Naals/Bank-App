5d23cd272a5afd807a12aceb1fb8d2c6dde56484
Aslan <tynyshtykaslan06@gmail.com>
2026-01-21 15:28:59 +0500

    project init

---
+++ src/main/java/com/project/banksystemapp/BankSystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.banksystemapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSystemAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/banksystemapp/BankSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.banksystemapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
