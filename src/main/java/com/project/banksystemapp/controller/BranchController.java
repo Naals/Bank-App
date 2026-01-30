d32ac7e2bb913540b1182a2424bad805c8ec1e6a
Aslan <tynyshtykaslan06@gmail.com>
2026-01-30 21:23:30 +0500

    branch controller init

---
+++ src/main/java/com/project/banksystemapp/controller/BranchController.java
@@ -0,0 +1,11 @@
+package com.project.banksystemapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/branches")
+@RequiredArgsConstructor
+public class BranchController {
+}
