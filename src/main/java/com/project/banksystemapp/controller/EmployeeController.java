afefaf5e5136b5354707ed180b3eab2caf152eda
Aslan <tynyshtykaslan06@gmail.com>
2026-02-06 23:43:21 +0500

    EmployeeController realized

--- src/main/java/com/project/banksystemapp/controller/EmployeeController.java
+++ src/main/java/com/project/banksystemapp/controller/EmployeeController.java
@@ -1,9 +1,15 @@
 package com.project.banksystemapp.controller;
 
+import com.project.banksystemapp.domain.UserRole;
+import com.project.banksystemapp.modal.User;
+import com.project.banksystemapp.payload.dto.UserDto;
+import com.project.banksystemapp.payload.response.ApiResponse;
 import com.project.banksystemapp.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -12,5 +18,60 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
+    @PostMapping("/store/{storeId}")
+    public ResponseEntity<UserDto> createStoreEmployee(
+            @PathVariable Long storeId,
+            @RequestBody UserDto userDto
+    ) {
+        UserDto employee = employeeService.createStoreEmployee(userDto, storeId);
+        return ResponseEntity.ok(employee);
+    }
 
+    @PostMapping("/branch/{branchId}")
+    public ResponseEntity<UserDto> createBranchEmployee(
+            @PathVariable Long branchId,
+            @RequestBody UserDto userDto
+    ) {
+        UserDto employee = employeeService.createBranchEmployee(userDto, branchId);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateEmployee(
+            @PathVariable Long id,
+            @RequestBody UserDto userDto
+    ) {
+        User employee = employeeService.updateEmployee(userDto, id);
+        return ResponseEntity.ok(employee);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<ApiResponse> deleteEmployee(
+            @PathVariable Long id
+    ) {
+        employeeService.deleteEmployee(id);
+
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setMessage("Employee deleted successfully");
+
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @GetMapping("/store/{id}")
+    public ResponseEntity<List<User>> storeEmployee(
+            @PathVariable Long id,
+            @RequestParam(required = false) UserRole role
+    ) {
+        List<User> employee = employeeService.findStoreEmployees(id, role);
+        return ResponseEntity.ok(employee);
+    }
+
+    @GetMapping("/branch/{id}")
+    public ResponseEntity<List<User>> branchEmployee(
+            @PathVariable Long id,
+            @RequestParam(required = false) UserRole role
+    ) {
+        List<User> employee = employeeService.findBranchEmployees(id, role);
+        return ResponseEntity.ok(employee);
+    }
 }
