f05d74c2b041884abcc73e0e6731324a61a16e84
Aslan <tynyshtykaslan06@gmail.com>
2026-02-06 23:34:41 +0500

    EmployeeController init

---
+++ src/main/java/com/project/banksystemapp/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.project.banksystemapp.controller;
+
+import com.project.banksystemapp.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+
+}
