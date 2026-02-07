265d9c029b64a6278b0c8928356bd3f9868c57b6
Aslan <tynyshtykaslan06@gmail.com>
2026-02-07 23:04:53 +0500

    Customer Service implementation

--- src/main/java/com/project/banksystemapp/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/project/banksystemapp/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,38 @@
 package com.project.banksystemapp.service.impl;
 
-public class CustomerServiceImpl {
+import com.project.banksystemapp.modal.Customer;
+import com.project.banksystemapp.service.CustomerService;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public Customer createCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer updateCustomer(Long id, Customer customer) {
+        return null;
+    }
+
+    @Override
+    public void deleteCustomer(Long id) {
+
+    }
+
+    @Override
+    public Customer getCustomer(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return List.of();
+    }
+
+    @Override
+    public List<Customer> searchCustomers(String keyword) {
+        return List.of();
+    }
 }
