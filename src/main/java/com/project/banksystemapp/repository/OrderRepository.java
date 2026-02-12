4346e0419e6fbe50d7563ecf404687d5636bdec0
Aslan <tynyshtykaslan06@gmail.com>
2026-02-12 22:58:27 +0500

    Order Repository realized

---
+++ src/main/java/com/project/banksystemapp/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.project.banksystemapp.repository;
+
+import com.project.banksystemapp.modal.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByCustomerId(Long customerId);
+    List<Order> findByBranchId(Long branchId);
+    List<Order> findByCashierId(Long cashierId);
+    List<Order> findByBranchIdAndCreatedAtBetween(Long branchId, LocalDateTime from, LocalDateTime to);
+}
