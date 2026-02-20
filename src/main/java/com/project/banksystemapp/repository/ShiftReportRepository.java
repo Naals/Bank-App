2a24aa4926ddbf8c035b3412efdb6125e8df09bc
Aslan <tynyshtykaslan06@gmail.com>
2026-02-20 22:39:58 +0500

    shiftReport repo realized

---
+++ src/main/java/com/project/banksystemapp/repository/ShiftReportRepository.java
@@ -0,0 +1,22 @@
+package com.project.banksystemapp.repository;
+
+import com.project.banksystemapp.modal.ShiftReport;
+import com.project.banksystemapp.modal.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface ShiftReportRepository extends JpaRepository<ShiftReport, Long> {
+
+    List<ShiftReport> findByCashierId(Long id);
+    List<ShiftReport> findByBranchId(Long id);
+
+    Optional<ShiftReport> findTopByCashierAndShiftEndIsNullOrderByShiftStartDesc(
+            User cashier);
+
+    Optional<ShiftReport> findByCashierAndShiftStartBetween(User cashier,
+                                                            LocalDateTime start,
+                                                            LocalDateTime end);
+}
