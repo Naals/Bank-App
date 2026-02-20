279d340bc9122267149151d0b44a5c43c5868b4f
Aslan <tynyshtykaslan06@gmail.com>
2026-02-20 22:17:34 +0500

    shift dto report

---
+++ src/main/java/com/project/banksystemapp/payload/dto/ShiftReportDto.java
@@ -0,0 +1,38 @@
+package com.project.banksystemapp.payload.dto;
+
+import com.project.banksystemapp.modal.Branch;
+import com.project.banksystemapp.modal.PaymentSummary;
+import com.project.banksystemapp.modal.Refund;
+import com.project.banksystemapp.modal.User;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShiftReportDto {
+
+    private Long id;
+    private LocalDateTime shiftStart;
+    private LocalDateTime shiftEnd;
+
+    private Double totalSales;
+    private Double totalRefunds;
+    private Double netSale;
+    private Double totalOrders;
+
+    private UserDto cashier;
+    private Long cashierId;
+    private String cashierName;
+
+    private BranchDto branch;
+    private Long branchId;
+    private String branchName;
+
+    private List<PaymentSummary> paymentSummaries;
+    private List<ProductDto> topSellingProducts;
+    private List<OrderDto> recentOrders;
+    private List<RefundDto> refunds;
+}
