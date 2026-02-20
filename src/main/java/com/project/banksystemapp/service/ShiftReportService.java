b4ef03b44842a1418f9b40d4317c6724508f8fdc
Aslan <tynyshtykaslan06@gmail.com>
2026-02-20 22:39:47 +0500

    shiftReport service realized

---
+++ src/main/java/com/project/banksystemapp/service/ShiftReportService.java
@@ -0,0 +1,26 @@
+package com.project.banksystemapp.service;
+
+import com.project.banksystemapp.exceptions.UserException;
+import com.project.banksystemapp.payload.dto.ShiftReportDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ShiftReportService {
+
+    ShiftReportDto startShift(Long cashierId, Long branchId,
+                              LocalDateTime shiftStart) ;
+
+    ShiftReportDto endShift(Long shiftReportId, LocalDateTime shiftEnd) ;
+
+    ShiftReportDto getShiftReportById(Long id);
+
+    List<ShiftReportDto> getAllShiftReports();
+
+    List<ShiftReportDto> getShiftReportsByBranchId(Long branchId);
+    List<ShiftReportDto> getShiftReportsByCashierId(Long cashierId);
+
+    ShiftReportDto getCurrentShiftProgress(Long cashierId) throws UserException;
+
+    ShiftReportDto getShiftByCashierAndDate(Long cashierId, LocalDateTime date) throws UserException;
+}
