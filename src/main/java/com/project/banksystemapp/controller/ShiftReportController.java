1f5fa2aed236c648ce744aac51914afed3cc8b19
Aslan <tynyshtykaslan06@gmail.com>
2026-02-22 13:28:35 +0500

    shift controller init

---
+++ src/main/java/com/project/banksystemapp/controller/ShiftReportController.java
@@ -0,0 +1,14 @@
+package com.project.banksystemapp.controller;
+
+import com.project.banksystemapp.service.ShiftReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/shift-reports")
+public class ShiftReportController {
+
+    private final ShiftReportService shiftReportService;
+}
