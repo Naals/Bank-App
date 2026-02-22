8489e75ef8ae6d11fb3f26cbcd5ce219d8c43f82
Aslan <tynyshtykaslan06@gmail.com>
2026-02-22 13:47:02 +0500

    shift controller half realized

--- src/main/java/com/project/banksystemapp/controller/ShiftReportController.java
+++ src/main/java/com/project/banksystemapp/controller/ShiftReportController.java
@@ -1,9 +1,16 @@
 package com.project.banksystemapp.controller;
 
+import com.project.banksystemapp.exceptions.UserException;
+import com.project.banksystemapp.payload.dto.ShiftReportDto;
 import com.project.banksystemapp.service.ShiftReportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +18,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShiftReportController {
 
     private final ShiftReportService shiftReportService;
+
+    @PostMapping("/start")
+    public ResponseEntity<ShiftReportDto> startShift() throws UserException {
+        return ResponseEntity.ok(
+                shiftReportService.startShift()
+        );
+    }
+
+    @PatchMapping("/end")
+    public ResponseEntity<ShiftReportDto> endShift() throws UserException {
+        return ResponseEntity.ok(
+                shiftReportService.endShift(null, null)
+        );
+    }
+
+    @GetMapping("/current")
+    public ResponseEntity<ShiftReportDto> getCurrentShiftProgress() throws UserException {
+        return ResponseEntity.ok(
+                shiftReportService.getCurrentShiftProgress(null)
+        );
+    }
+
+    @GetMapping("/cashier/{cashierId}/current")
+    public ResponseEntity<ShiftReportDto> getShiftReportByDate(
+            @PathVariable Long cashierId,
+            @RequestParam @DateTimeFormat (iso = DateTimeFormat.ISO.DATE) LocalDateTime startDate
+    ) throws UserException {
+        return ResponseEntity.ok(
+                shiftReportService.getShiftByCashierAndDate(cashierId, startDate)
+        );
+    }
 }
